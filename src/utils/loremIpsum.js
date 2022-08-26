a1526ff286a5751589f166aca3359a1387e7b72f
kovalm <kovelov@gmail.com>
2022-08-26 20:38:51 -0300

    Lorem ipsum dictionary

---
+++ src/utils/loremIpsum.js
@@ -0,0 +1,17 @@
+export const LOREM_110 =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.";
+
+export const LOREM_115 =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi, quam nec venenatis lobortis, mi risus tempus nulla.";
+
+export const LOREM_124 =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi risus tempus nulla.";
+
+export const LOREM_157 =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi risus tempus nulla, sed porttitor est nibh at nulla.";
+
+export const LOREM_230 =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbisagittis, quam nec venenatis lobortis, mi risus tempus nulla,sed porttitor est nibh at nulla. Praesent placerat enim ut extincidunt vehicula. Fusce sit amet dui tellus.";
+
+export const LOREM_233 =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi risus tempus nulla, sed porttitor est nibh at nulla. Praesent placerat enim ut ex tincidunt vehicula. Fusce sit amet dui tellus.";
